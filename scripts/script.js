dea2c90b9e40d7101b5fed163ead80afd5425b0e
CHICHA <thachicha@yandex.ru>
2020-07-09 22:31:16 +0900

    feat: добавлен script, адекватная работа popup

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,60 @@
-const popup = document.querySelector('.popup')
-const popupOpenButton = document.querySelector('.profile__info-editbutton')
-const popupCloseButton = popup.querySelector('.popup__close')
-const header = document.querySelector('.popup_opened')
 
-const popupToggle = function (event) {
-  console.log('button clicked')
-  event.stopPropagation()
-  popup.classList.toggle('popup_opened')
+console.log('скрипт подключен');
+
+
+const profileInfo = document.querySelector('.profile__info');
+const editButton = profileInfo.querySelector('.profile__open-popup-editor');
+const profileName = profileInfo.querySelector('.profile__name');
+const profileCaption = profileInfo.querySelector('.profile__caption');
+
+
+const popup = document.querySelector('.popup');
+const closeButton = popup.querySelector('.popup__close-popup-editor');
+const formElement = popup.querySelector('.popup__form');
+const nameInput = popup.querySelector('.popup__name')
+const captionInput = popup.querySelector('.popup__caption')
+const overlay = popup.querySelector('.popup__overlay')
+
+
+
+function popupOpen() {
+  popup.classList.add('popup_opened')
+  console.log('попап открыт');
 }
 
-const closePopupByClickingOverlay = function (event) {
-  if (event.target !== event.currentTarget) { return }
-  popupToggle()
+function popupClose() {
+  popup.classList.remove('popup_opened')
+  console.log('попап закрыт');
 }
 
-popupOpenButton.addEventListener('click', popupToggle)
-popupCloseButton.addEventListener('click', popupToggle)
-popup.addEventListener('click', closePopupByClickingOverlay)
+function popupBio(userName, userCaption) {
+  nameInput.value = userName;
+  captionInput.value = userCaption;
 
-header.addEventListener('click', function () {
-  console.log('header clicked')
-})
+  console.log("popupBio " + userName)
+  console.log("popupBio " + userCaption)
+
+  popupOpen();
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileCaption.textContent = captionInput.value;
+
+  console.log("formSubmitHandler " + nameInput.value)
+  console.log("formSubmitHandler " + captionInput.value)
+  console.log("formSubmitHandler " + profileName.textContent)
+  console.log("formSubmitHandler " + profileCaption.textContent)
+
+  popupClose();
+}
+
+
+editButton.addEventListener('click', () => popupBio(profileName.textContent, profileCaption.textContent));
+
+closeButton.addEventListener('click', () => popupClose());
+overlay.addEventListener('click', () => popupClose());
+
+formElement.addEventListener('submit', formSubmitHandler);
